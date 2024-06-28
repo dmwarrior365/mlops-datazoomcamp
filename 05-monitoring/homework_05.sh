a0c22a57d0d5a210660566b7de6e6c90c41ecd55
Redot's Learn <74037003+dmwarrior365@users.noreply.github.com>
2024-06-28 09:47:29 +0000

    add new folder for homework 05

---
+++ 05-monitoring/homework_05.sh
@@ -0,0 +1,8 @@
+# Question-1
+The number of data is 57457 from the Green Taxi March 2024
+# Question-2
+I chose QuantileMetric
+# Question-3
+The maximum value of quantile is 14.2 for March 2024
+# Question-4
+This is the location of the save dashboard in Grafana: project_folder/config (05-monitoring/config)
